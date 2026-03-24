344499c7a0d62914ab3779dc439b48e4d384fb6f
majy2016 <372840680@qq.com>
2026-03-25 01:16:50 +0800

    feat: 新增会议管理 API URL 常量

---
+++ variable/manage_var.py
@@ -0,0 +1,14 @@
+"""会议管理 API URL 常量。"""
+
+
+class ManageVar:
+    """会议管理 API 常量。"""
+
+    # 基础 URL
+    BASE_URL = "https://meeting.huaweicloud.com"
+
+    # 登录接口
+    LOGIN_URL = f"{BASE_URL}/v2/usg/acs/auth/account"
+
+    # 会议管理接口
+    CONFERENCE_URL = f"{BASE_URL}/v1/mmc/management/conferences"
